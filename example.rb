91b14898c5a46317219fcad798abc752a12e25a4
Dave Miller <davidjmiller24@gmail.com>
2017-01-05 22:34:09 -0500

    fixed one line of extra indentation in example.rb

--- example.rb
+++ example.rb
@@ -9,7 +9,7 @@ print <<"EOF";
       - Max Height (feet): #{gpx.elevations.max.feet}
       - Max Height (meters): #{gpx.elevations.max.meters}
       - Min Height (feet): #{gpx.elevations.min.feet}
-       - Min Height (meters): #{gpx.elevations.min.meters}
+      - Min Height (meters): #{gpx.elevations.min.meters}
       - Elevation Change (feet): #{gpx.elevations.change.feet}
       - Elevation Change (meters): #{gpx.elevations.change.meters}
     Trip Details:
